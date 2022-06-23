bda73915b159349cb3724b186c92a2eca3095fea
kondelow <achmadrasidi@gmail.com>
2022-06-23 14:59:38 +0700

    update

--- src/routes/user.js
+++ src/routes/user.js
@@ -1,12 +1,11 @@
 const Router = require("express").Router();
 const userController = require("../controller/user");
-const {checkToken} =require('../middlewares/auth')
-const imageUpload =require('../middlewares/upload')
-
-
+const { checkToken } = require("../middlewares/auth");
+const imageUpload = require("../middlewares/upload");
 
 //Router User
-Router.get("/",checkToken, userController.getUserInfo); 
-Router.patch("/",checkToken, imageUpload.single("photo"), userController.patchUserInfo);
+Router.get("/", checkToken, userController.getUserInfo);
+Router.patch("/", checkToken, imageUpload.single("photo"), userController.patchUserInfo);
+//update
 
 module.exports = Router;
