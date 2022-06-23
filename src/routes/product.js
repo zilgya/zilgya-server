b4c29109a9b75be3c6b4fd02e930102abb2f7fc1
kondelow <achmadrasidi@gmail.com>
2022-06-23 12:40:55 +0700

    add update product

--- src/routes/product.js
+++ src/routes/product.js
@@ -1,10 +1,11 @@
 const Router = require("express").Router();
-const {checkToken} =require('../middlewares/auth')
-const imageUpload =require('../middlewares/upload')
+const { checkToken } = require("../middlewares/auth");
+const imageUpload = require("../middlewares/upload");
 
-const {createProduct} = require('../controller/product')
+const { createProduct, removeProduct, patchProduct } = require("../controller/product");
 
-Router.post('/',checkToken,imageUpload.array('photo',5),createProduct)
+Router.post("/", checkToken, imageUpload.array("photo", 5), createProduct);
+Router.patch("/:id", checkToken, imageUpload.array("photo", 5), patchProduct);
+Router.delete("/:id", checkToken, removeProduct);
 
-
-module.exports = Router
+module.exports = Router;
