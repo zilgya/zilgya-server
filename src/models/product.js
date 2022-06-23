831f7d307af9e9dc01eeb768062c2fafd3707350
Lukmandst <lukmandwi11@gmail.com>
2022-06-23 14:09:09 +0700

    fix typo

--- src/models/product.js
+++ src/models/product.js
@@ -19,7 +19,7 @@ const getProducts = (query, route) => {
     let totalQuery =
       "select count(*) over() as total_products from products p join categories c on p.categories_id =c.id join users u on p.users_id = u.id join brands b on p.brands_id =b.id join colors c2 on p.colors_id =c2.id";
     let sqlQuery =
-      "SELECT p.name, p.description, p.price, p.stock, p.stock_condition, c.name as category, b.name as brand, c2.name as color, u.id as seller_id FROM products p join categories c on p.categories_id =c.id join users u on p.users_id = u.id join brands b on p.brands_id =b.idjoin colors c2 on p.colors_id =c2.id where p.on_delete = false";
+      "SELECT p.name, p.description, p.price, p.stock, p.stock_condition, c.name as category, b.name as brand, c2.name as color, u.id as seller_id FROM products p join categories c on p.categories_id =c.id join users u on p.users_id = u.id join brands b on p.brands_id =b.id join colors c2 on p.colors_id =c2.id where p.on_delete = false";
     if (!find && !categories) {
       sqlQuery += " order by " + sort + " " + order + " LIMIT $1 OFFSET $2";
       arr.push(parseInt(limit), offset);
