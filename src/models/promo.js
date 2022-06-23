507ced262a908aa5a6c1e0c1deece64b445859c1
kondelow <achmadrasidi@gmail.com>
2022-06-23 17:02:53 +0700

    fix promo query

--- src/models/promo.js
+++ src/models/promo.js
@@ -1,28 +1,26 @@
-const { response } = require("express");
 const { db } = require("../config/database");
 
 const getPromo = (body) => {
-    return new Promise((resolve, reject) => {
-        const { code } = body;
-        let sqlQuery =
-            "select * from promos where lower(code) like lower('%' || $1 || '%') "; 
-        db.query(sqlQuery, [code])
-            .then((result) => {
-                if (result.rows.length === 0) {
-                    return reject({ status: 404, err: "Promo Not Found" });
-                }
-                const response = {
-                    total: result.rowCount,
-                    data: result.rows,
-                };
-                resolve(response);
-            })
-            .catch((err) => {
-                reject({ status: 500, err });
-            })
-    });
+  return new Promise((resolve, reject) => {
+    const { code } = body;
+    let sqlQuery = "select * from promos where lower(code)=$1 ";
+    db.query(sqlQuery, [code])
+      .then((result) => {
+        if (result.rows.length === 0) {
+          return reject({ status: 404, err: "Promo Not Found" });
+        }
+        const response = {
+          total: result.rowCount,
+          data: result.rows,
+        };
+        resolve(response);
+      })
+      .catch((err) => {
+        reject({ status: 500, err });
+      });
+  });
 };
 
 module.exports = {
-    getPromo
-}
+  getPromo,
+};
